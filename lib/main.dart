6393dda23ebca0563756c7f2e22463ca257a698f
Harshith Paladi <87645227+HarshithPaladi@users.noreply.github.com>
2023-06-06 23:40:51 +0530

    [Update] Use Device location for weather

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
-
+import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-void main() {
-  runApp(const Weather());
+Future main() async {
+  await dotenv.load(fileName: ".env");
+  runApp(Weather());
 }
 
 class Weather extends StatefulWidget {
@@ -17,98 +19,139 @@ class Weather extends StatefulWidget {
 
 class _WeatherState extends State<Weather> {
   var temp;
-  var city = "London";
-  Future getWeather() async {
+  var city = "";
+  var searchController = TextEditingController();
+
+  Future getWeatherByCity(String city) async {
+    final apiKey = dotenv.env['API_KEY'];
+
     http.Response response = await http.get(Uri.parse(
-        'https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=89727fd78420e93cc55a572c11542774&units=metric'));
+        'https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$apiKey&units=metric'));
 
     var results = jsonDecode(response.body);
     setState(() {
       this.temp = results['main']['temp'];
+      this.city = results['name'];
     });
+  }
+
+  Future getWeather(double latitude, double longitude) async {
+    final apiKey = dotenv.env['API_KEY'];
+
+    http.Response response = await http.get(Uri.parse(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey&units=metric'));
+
+    var results = jsonDecode(response.body);
+    setState(() {
+      this.temp = results['main']['temp'];
+      this.city = results['name'];
+    });
+  }
+
+  Future<void> getLocationAndWeather() async {
+    final permissionStatus = await Permission.locationWhenInUse.request();
+    if (permissionStatus.isGranted) {
+      Position position;
+      try {
+        position = await Geolocator.getCurrentPosition(
+            desiredAccuracy: LocationAccuracy.high);
+        getWeather(position.latitude, position.longitude);
+      } catch (e) {
+        print('Error: $e');
+      }
+    }
   }
 
   @override
   void initState() {
     super.initState();
-    this.getWeather();
+    getLocationAndWeather();
+  }
+
+  @override
+  void dispose() {
+    searchController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // return MaterialApp(
-    //   title: 'Weather From API',
-    //   home: Scaffold(
-    //     body: Column(children: [
-    //       Container()
-    //     ]),
-    //   ),
-    // );
-
     return MaterialApp(
-        title: 'Weather App',
-        home: Scaffold(
-          body: Column(
-            children: [
-              Container(
-                height: MediaQuery.of(context).size.height / 3,
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        colors: [
-                          Colors.blueAccent,
-                          Colors.lightBlueAccent,
-                        ]),),
-                // color: Colors.red,
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 10.0),
-                        child: Text(
-                          'Currently in ${city}',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 14.0,
-                              fontWeight: FontWeight.w600),
-                        ),
-                      ),
-                      Text(
-                        temp != null ? temp.toString() + "\u00B0" : "Loading",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 40.0,
-                            fontWeight: FontWeight.w600),
-                      ),
-                    ]),
+      title: 'Weather App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Weather App'),
+        ),
+        body: Column(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height / 3,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Colors.blueAccent,
+                    Colors.lightBlueAccent,
+                  ],
+                ),
               ),
-
-                  Container(
-                    // height: MediaQuery.of(context).size.height / 3,
-                    // width: MediaQuery.of(context).size.width/2,
-                    padding: EdgeInsets.all(20.0),
-                    child: TextField(
-                      decoration: InputDecoration(
-                          hintText: 'Enter City Name',
-                          prefixIcon: Icon(Icons.search),
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10.0))),
-                      onSubmitted: (value) {
-                        setState(() {
-                          this.city = value;
-                          this.getWeather();
-                        });
-                      },
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 10.0),
+                    child: Text(
+                      'Currently in ${city}',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.w600,
+                      ),
                     ),
                   ),
-                  // IconButton(onPressed: getWeather, icon: Icon(Icons.search))
+                  Text(
+                    temp != null ? temp.toString() + "\u00B0" : "Loading",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
                 ],
-          ),
-        ));
+              ),
+            ),
+            SizedBox(height: 20),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              child: TextField(
+                controller: searchController,
+                decoration: InputDecoration(
+                  hintText: 'Enter City Name',
+                  prefixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                getWeatherByCity(searchController.text);
+              },
+              child: Text('Search'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: getLocationAndWeather,
+              child: Text('Get Current Location'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
-
-  get tempColor => ;
 }
